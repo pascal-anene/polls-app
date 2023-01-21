f69b76be9ffd9456563805cd0624fe99cd203421
Pascal Anene <pascalanene@Pascals-MacBook-Pro.local>
2023-01-21 07:38:06 +0100

    amending index view to use generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-
 from django.http import HttpResponse # No longer needed since we have HttpResponseRedirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
@@ -11,10 +11,15 @@ from .models import Choice, Question
 # The Index view
 # Initially we used the template loader before 
 # implementing the shortcut using render() function
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list,} 
-    return render(request, "polls/index.html", context)
+# Now we switched to using Django's Generic Views
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 # The Detail view
 def detail(request, question_id):
