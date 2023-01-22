70dcffaa5fdd5ceffaa6a201408e9b37e0cc335d
Pascal Anene <pascalanene@Pascals-MacBook-Pro.local>
2023-01-22 07:59:41 +0100

    refactored admin panel to include Choice in Question model

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,16 +2,21 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 # Customizing the default admin form
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes":["collapse"]}),
     ]
+    inlines = [ChoiceInLine]
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
 
-admin.site.register(Choice)
 
 
