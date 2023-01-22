1d3fda8cd8030694fe5f9a55707971872cd41a33
Pascal Anene <pascalanene@Pascals-MacBook-Pro.local>
2023-01-22 07:49:19 +0100

    customizing admin panel form fields

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Question
 
+# Customizing the default admin form
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
+
 
